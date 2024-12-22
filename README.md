2546c891d4da615367358ef0fb13e693dd57e87d
Thomas Milton <perso.milton@gmail.com>
2024-12-22 16:46:56 +0100

    first img importation and first animation

--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Tuto from YouTube : © Clear Code : Master Python by making 5 games [the new ult
  https://www.youtube.com/watch?v=8OMghdHP-zs&t=144s
 
  2024 - 2025
+ 
--- code/main.py
+++ code/main.py
@@ -1,18 +1,32 @@
 import pygame
 
 # General Setup
-pygame.init()
+pygame.init() # Initialize the game
 WINDOW_WITH,WINDOW_HEIGHT = 1280, 720
 display_surface =  pygame.display.set_mode((WINDOW_WITH,WINDOW_HEIGHT))
+pygame.display.set_caption('Space shooter')
 running = True
- 
+
+# Plain Surface
+surf = pygame.Surface((100, 200)) 
+surf.fill('orange')
+x = 100 
+
+# Importing an image
+player_surface = pygame.image.load('images/player.png')
 
 while running:
     # create loop : inside for keyboard input, mouse input, timers, UI interaction...
     for event in pygame.event.get():
-        # 
+        # Quit the game
         if event.type == pygame.QUIT:
             running = False
 
-
     # draw the game
+    display_surface.fill('darkgrey')
+    x += 0.1
+    display_surface.blit(player_surface, (x, 150)) # blit link 2 surfaces : surface, target position begin to top left
+    pygame.display.update() # update() entire window when flip update a part of a window 
+
+pygame.quit() # <> init so we shutdown the game properly
+
