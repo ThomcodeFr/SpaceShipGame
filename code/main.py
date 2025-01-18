f53d20093373c7947fad40108418bf163ba0c89d
Thomas Milton <perso.milton@gmail.com>
2025-01-18 21:37:07 +0100

    add game surfaces

--- code/main.py
+++ code/main.py
@@ -1,4 +1,6 @@
 import pygame
+from os.path import join
+from random import randint
 
 # General Setup
 pygame.init() # Initialize the game
@@ -8,12 +10,12 @@ pygame.display.set_caption('Space shooter')
 running = True
 
 # Plain Surface
-surf = pygame.Surface((100, 200)) 
-surf.fill('orange')
 x = 100 
 
-# Importing an image
-player_surface = pygame.image.load('images/player.png')
+# Importing images
+player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
+star_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
+star_position = [(randint(0, WINDOW_WITH), randint(0, WINDOW_HEIGHT)) for i in range(20)]
 
 while running:
     # create loop : inside for keyboard input, mouse input, timers, UI interaction...
@@ -22,11 +24,18 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # draw the game
+    # draw the game in order 
+    # Wallpaper
     display_surface.fill('darkgrey')
+    # Stars 
+    for pos in star_position:
+        display_surface.blit(star_surface, pos)
     x += 0.1
+    # Ship 
     display_surface.blit(player_surface, (x, 150)) # blit link 2 surfaces : surface, target position begin to top left
+    
     pygame.display.update() # update() entire window when flip update a part of a window 
 
+    
 pygame.quit() # <> init so we shutdown the game properly
 
