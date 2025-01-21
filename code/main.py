aa159b56b1081f2ea0012269beb7f451a2f554f2
Thomas Milton <perso.milton@gmail.com>
2025-01-21 20:16:53 +0100

    Moving ship with keyboard

--- code/main.py
+++ code/main.py
@@ -18,7 +18,7 @@ x = 100
 # Importing images
 player_surface = pygame.image.load(join('images', 'player.png')).convert_alpha()
 player_rect = player_surface.get_frect(center = (WINDOW_WITH / 2, WINDOW_HEIGHT / 2))
-player_direction = pygame.math.Vector2(1, 1 )
+player_direction = pygame.math.Vector2()
 player_speed = 300 
 
 star_surface = pygame.image.load(join('images', 'star.png')).convert_alpha()
@@ -38,9 +38,22 @@ while running:
         # Quit the game
         if event.type == pygame.QUIT:
             running = False
+        # if event.type == pygame.KEYDOWN and event.key == pygame.K_1:
+        #    print(1)
+        # if event.type == pygame.MOUSEMOTION:
+        #    player_rect.center = event.pos
+
+    # input
+    keys = pygame.key.get_pressed()
+    player_direction.x = int(keys[pygame.K_RIGHT]) - int(keys[pygame.K_LEFT])
+    player_direction.y = int(keys[pygame.K_DOWN]) - int(keys[pygame.K_UP])
+    player_direction = player_direction.normalize() if player_direction else player_direction
+    print((player_direction * player_speed).magnitude())
+    
+    player_rect.center += player_direction * player_speed * dt 
+
 
     # draw the game in order 
-
     # Wallpaper
     display_surface.fill('darkgrey')
 
@@ -51,15 +64,8 @@ while running:
     # Showing element on screen
     display_surface.blit(meteor_surface, meteor_rect)
     display_surface.blit(laser_surface, laser_rect)
-    
-    # Player movement
-    if player_rect.bottom >= WINDOW_HEIGHT or player_rect.top <= 0:
-        player_direction.y *= -1
-    if player_rect.right >= WINDOW_WITH or player_rect.left <= 0:
-        player_direction.x *= -1
-
-    player_rect.center += player_direction * player_speed * dt
     display_surface.blit(player_surface, player_rect)
+    
     
     pygame.display.update() # update() entire window when flip update a part of a window 
     
